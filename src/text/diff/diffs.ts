34af584250ed160b24f35a1f19c9163c4878ef3f
elzup <guild0105@gmail.com>
2023-11-30 22:47:04 +0900

    add diff checker

--- src/text/diff/diffs.ts
+++ src/text/diff/diffs.ts
@@ -1,16 +1,45 @@
 import { readFile } from 'fs/promises'
 import { diffChars } from 'diff'
 
+const replacements = {
+	a: 'A',
+}
+
+const kanaToHiragana = (str) =>
+	str.replace(/[\u30A1-\u30F6]/g, (match) =>
+		String.fromCharCode(match.charCodeAt(0) - 0x60)
+	)
+
+const zenkakuToHankaku = (str) =>
+	str
+		.replace(/[\uff01-\uff5e]/g, (ch) =>
+			String.fromCharCode(ch.charCodeAt(0) - 0xfee0)
+		)
+		.replace(/\u3000/g, ' ')
+
+const normalizeNumbers = (str: string) =>
+	str.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0))
+const normalize = (s: string) =>
+	Object.entries(replacements).reduce(
+		(acc, [key, value]) => acc.replaceAll(key, value),
+		zenkakuToHankaku(normalizeNumbers(kanaToHiragana(s)))
+			.toLowerCase()
+			.replace(/[、。／/・　 「」?]/g, '')
+	)
+
 function calculateDiffAndExtractChanges(s1: string, s2: string) {
-	const diffResult = diffChars(s1, s2)
+	const diffResult = diffChars(normalize(s1), normalize(s2))
 
 	let diffScore = 0
 	let changes = ''
 
 	diffResult.forEach((part) => {
-		diffScore += part.count ?? 0
-		if ((part.added ?? false) || (part.removed ?? false)) {
-			changes += '|' + part.value
+		if (part.added ?? false) {
+			changes += '+' + part.value
+			diffScore += part.count ?? 0
+		} else if (part.removed ?? false) {
+			changes += '-' + part.value
+			diffScore += part.count ?? 0
 		} else {
 			changes += '.'.repeat(Math.log2(part.count ?? 0))
 		}
@@ -32,7 +61,9 @@ async function main() {
 			valsB[i]
 		)
 
+		// if (diffScore > 0) {
 		console.log(diffScore + '\t' + changes)
+		// }
 	}
 }
 
