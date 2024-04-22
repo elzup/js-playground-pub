a9afd0338ef8b98de2856a60bd559459c2fbb752
elzup <guild0105@gmail.com>
2024-04-22 23:13:51 +0900

    update

--- src/math/digit_uniq.ts
+++ src/math/digit_uniq.ts
@@ -2,19 +2,16 @@ const l = 10000000000
 const start = l / 10
 const uniqueDigits = (s) => new Set(s.split('')).size
 
-const counter = new Map()
+const counter = new Map<number, number>()
 
-const progress = (current: number, total: number, hash: string) => {
-	const percentage = (current / total) * 100
+const progress = (counter: Map<number, number>) => {
 	process.stdout.clearLine(0)
 	process.stdout.cursorTo(0)
-	const bar =
-		Array(Math.floor(percentage / 3))
-			.fill('=')
-			.join('') +
-		'>'.padEnd(30 - Math.floor(percentage / 3)) +
-		` ${current}/${total} `
-	process.stdout.write(`${bar} ${percentage.toFixed(2)}%`)
+	process.stdout.write(
+		Array.from(counter)
+			.map(([k, v]) => `${k}: ${v}`)
+			.join(', ')
+	)
 }
 
 function main() {
@@ -23,7 +20,7 @@ function main() {
 		counter.set(uniqueDigits(s), (counter.get(uniqueDigits(s)) || 0) + 1)
 
 		if (i % 7654 === 0) {
-			progress(i, l, s)
+			progress(counter)
 		}
 	}
 	console.log(counter)
